242f105db1be3af205c5b2f149da989f60233128
Darkatek7 <50767771+Darkatek7@users.noreply.github.com>
2025-11-10 09:32:51 +0100

    Add constants for netduma_r3 component

---
+++ custom_components/netduma_r3/const.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+DOMAIN = "netduma_r3"
+DEFAULT_SCAN_INTERVAL = 20  # seconds for device presence
+DEFAULT_TREE_INTERVAL = 20  # seconds for traffic trees
+DEFAULT_SYS_INTERVAL = 90   # seconds for system info
+
+APP_DEVICES = "com.netdumasoftware.devicemanager"
+APP_QOS = "com.netdumasoftware.smartqos"
+APP_SYS = "com.netdumasoftware.systeminfo"
